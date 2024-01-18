f704d97e9e70f98c7c23bde33bbd33fb82bad253
danilogv <danilo.gv@hotmail.com>
2024-01-18 11:20:33 -0300

    Create project spring

---
+++ src/test/java/com/danilo/springbootkafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
